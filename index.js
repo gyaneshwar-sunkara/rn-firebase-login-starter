38c2741f54b96f19fb3745b4cf5f456b59a4435a
Gyani Sunkara <gyanisunkara1@gmail.com>
2022-05-22 02:20:12 +0530

    Necessary dependecy setup

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onRegister: function (data) {
+    const token = data.token;
+    console.log('TOKEN:', token);
+  },
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  onAction: function (notification) {
+    console.log('ACTION:', notification.action);
+    console.log('NOTIFICATION:', notification);
+  },
+  onRegistrationError: function (err) {
+    console.error('Notification error', err.message, err);
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
+});
+
 AppRegistry.registerComponent(appName, () => App);
